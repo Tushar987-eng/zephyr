11e693c53c164b959936358d5b45fadfa3b9fef6
Tushar Srivastav <tusharsrivastav2001@gmail.com>
2026-09-20 17:18:08 +0530

    Assignment 6 : Add LED sensor driver (our,driver) with sample_fetch/channel_get
    - Out-of-tree sensor driver: fetch turns LED on, channel_get reports state and turns it off
    - Devicetree binding (dts/bindings/our,driver.yaml), node with gpios in board overlay
    - App drives the LED only through the sensor API; verified on QEMU
    
    Signed-off-by: Tushar Srivastav <tusharsrivastav2001@gmail.com>

---
+++ app/drivers/sensor/driver.c
@@ -0,0 +1,72 @@
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
+
+#define DT_DRV_COMPAT our_driver
+
+LOG_MODULE_REGISTER(our_driver, LOG_LEVEL_INF);
+
+struct our_driver_config {
+    struct gpio_dt_spec led;
+};
+
+struct our_driver_data {
+    bool on;
+};
+
+static int our_sensor_sample_fetch(const struct device *dev, enum sensor_channel chan)
+{
+    LOG_INF("Hello! We are trying to do sensor_sample_fetch with channel : %d, this will move the LED to ON",chan);
+    const struct our_driver_config *cfg = dev->config;
+    struct our_driver_data *data = dev->data;
+    int ret = gpio_pin_set_dt(&cfg->led, 1);
+    if(ret == 0)
+        data->on = true;
+    return ret;
+}
+
+static int our_sensor_channel_get(const struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+{
+    if (val == NULL) {
+        return -EINVAL;
+    }
+    LOG_INF("Hello! We are trying to do sensor_channel_get with channel : %d, this will move the LED to OFF",chan);
+    const struct our_driver_config *cfg = dev->config;
+    struct our_driver_data *data = dev->data;
+    val->val1 = data->on;
+    val->val2 = 0;
+    int ret = gpio_pin_set_dt(&cfg->led, 0);
+    if(ret == 0)
+        data->on = false;
+    return ret;
+}
+
+static DEVICE_API(sensor, custom_sensor_api) = {
+    .sample_fetch = our_sensor_sample_fetch,
+	.channel_get = our_sensor_channel_get,
+};
+
+static int init(const struct device* dev)
+{
+    const struct our_driver_config *cfg = dev->config;
+
+    if (!gpio_is_ready_dt(&cfg->led)) 
+        return -ENODEV;
+
+    if (gpio_pin_configure_dt(&cfg->led, GPIO_OUTPUT_INACTIVE) < 0) 
+        return -1;
+
+    LOG_INF("Driver is Initialized!!!!!\n");
+    return 0;
+}
+
+#define OUR_DRIVER_DEFINE(inst)                                 \
+    static struct our_driver_data data_##inst;                  \
+    static const struct our_driver_config cfg_##inst = {        \
+        .led = GPIO_DT_SPEC_INST_GET(inst , gpios),             \
+    };                                                          \
+    SENSOR_DEVICE_DT_INST_DEFINE(inst, init, NULL,              \
+        &data_##inst, &cfg_##inst, POST_KERNEL,                 \
+        CONFIG_SENSOR_INIT_PRIORITY, &custom_sensor_api);       \
+
+DT_INST_FOREACH_STATUS_OKAY(OUR_DRIVER_DEFINE)
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,6 +1,7 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/sensor.h>
 
 #define SLEEP_TIME_MS CONFIG_APP_HEARTBEAT_PERIOD_MS
 
@@ -11,20 +12,45 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
+namespace {
+    void test() {
+        const struct device* driver = DEVICE_DT_GET(DT_NODELABEL(our_driver0));
+        struct sensor_value val;
+        auto ret = sensor_channel_get(driver, SENSOR_CHAN_AMBIENT_TEMP, &val);
+        LOG_INF("Channle ret : %d\n",ret); 
+    }
+}
+
 int main(void)
 {
-    bool led_state = true;
-
-    if (!gpio_is_ready_dt(&led)) return 0;
-
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
-
-    while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
-
-        led_state = !led_state;
-        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
+    const struct device* driver = DEVICE_DT_GET(DT_NODELABEL(our_driver0));
+    struct sensor_value val;
+    if(!device_is_ready(driver))
+    {
+        LOG_INF("Driver is not ready to function");
+        return 0;
+    }
+    while(1) {
+        auto ret = sensor_sample_fetch(driver);
+        LOG_INF("fetch -> %d (LED ON)", ret);
+        k_msleep(SLEEP_TIME_MS);
+        ret = sensor_channel_get(driver, SENSOR_CHAN_ALL, &val);
+        LOG_INF("get -> %d, was_on=%d (LED OFF)", ret, val.val1);
         k_msleep(SLEEP_TIME_MS);
     }
-    return 0;
+    // test();
+    // bool led_state = true;
+
+    // if (!gpio_is_ready_dt(&led)) return 0;
+
+    // if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+
+    // while (1) {
+    //     if (gpio_pin_toggle_dt(&led) < 0) return 0;
+
+    //     led_state = !led_state;
+    //     LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
+    //     k_msleep(SLEEP_TIME_MS);
+    // }
+    // return 0;
 }
